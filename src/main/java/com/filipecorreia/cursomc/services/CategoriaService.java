a6502b8e2c386b9eebc188cb0a9a18475ef2de10
Filipe Correia <correia481@gmail.com>
2021-01-13 17:39:09 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/filipecorreia/cursomc/services/CategoriaService.java
+++ src/main/java/com/filipecorreia/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.filipecorreia.cursomc.domain.Categoria;
+import com.filipecorreia.cursomc.dto.CategoriaDTO;
 import com.filipecorreia.cursomc.repositories.CategoriaRepository;
 import com.filipecorreia.cursomc.services.exception.DataIntegrityException;
 import com.filipecorreia.cursomc.services.exception.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
