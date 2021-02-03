63b114acb0f4aca110e1e62ed24ffd2b46c93a97
Filipe Correia <correia481@gmail.com>
2021-02-03 18:20:04 -0300

    Email HTML

--- src/main/java/com/filipecorreia/cursomc/services/MockEmailService.java
+++ src/main/java/com/filipecorreia/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.filipecorreia.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,7 +12,15 @@ public class MockEmailService extends AbstractEmailService {
 	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
-		LOG.info("simulando envio de amil...");
+		LOG.info("simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("simulando envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
