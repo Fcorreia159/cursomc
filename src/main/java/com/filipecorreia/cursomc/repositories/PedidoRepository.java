39930671cd91e0e193fee54a8c749c4694ef4d65
Filipe Correia <correia481@gmail.com>
2021-02-08 15:29:25 -0300

    Restriçao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/filipecorreia/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/filipecorreia/cursomc/repositories/PedidoRepository.java
@@ -1,10 +1,20 @@
 package com.filipecorreia.cursomc.repositories;
 
-import org.springframework.stereotype.Repository;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.filipecorreia.cursomc.domain.Cliente;
 import com.filipecorreia.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
-
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+		
+	
 }
--- src/main/java/com/filipecorreia/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/filipecorreia/cursomc/resources/ProdutoResource.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filipecorreia.cursomc.domain.Produto;
-import com.filipecorreia.cursomc.dto.CategoriaDTO;
 import com.filipecorreia.cursomc.dto.ProdutoDTO;
 import com.filipecorreia.cursomc.resources.utils.URL;
 import com.filipecorreia.cursomc.services.ProdutoService;
